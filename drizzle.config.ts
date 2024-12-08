b5ce78bbab3b1abdcd82eb21b860e3ee26f2fb39
Clayton Farr <contact@claytonfarr.com>
2024-12-08 09:24:35 -0700

    update to match upstream justship changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,9 @@
 import type { Config } from 'drizzle-kit'
 export default {
-	schema: './src/lib/server/database/schema.ts',
-	out: './drizzle',
-	dialect: 'sqlite',
-	driver: 'turso',
-	dbCredentials: {
-		url: 'file:local.db'
-	}
-} satisfies Config;
+  schema: './src/lib/server/database/schema.ts',
+  out: './drizzle',
+  dialect: 'turso',
+  dbCredentials: {
+    url: 'file:local.db',
+  },
+} satisfies Config
