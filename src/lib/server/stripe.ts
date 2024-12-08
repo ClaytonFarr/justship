cefe7eac9fb22a7f7dec3bae97bde2129c7cc752
Clayton Farr <contact@claytonfarr.com>
2024-12-08 11:59:51 -0700

    restore stripe test key

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -1,7 +1,9 @@
 import Stripe from 'stripe'
-import { STRIPE_SECRET_KEY } from '$env/static/private'
+import { STRIPE_SECRET_KEY, STRIPE_SECRET_KEY_TEST } from '$env/static/private'
 
-export const stripeClient = new Stripe(STRIPE_SECRET_KEY, {
+const api_key = import.meta.env.PROD ? STRIPE_SECRET_KEY : STRIPE_SECRET_KEY_TEST
+
+export const stripeClient = new Stripe(api_key, {
   apiVersion: '2024-11-20.acacia',
   httpClient: Stripe.createFetchHttpClient(),
 })
